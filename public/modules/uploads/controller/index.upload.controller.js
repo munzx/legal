d0c77af5121f90c8653e4d855842bddfffde7ff5
munzir <munzir.suliman@outlook.com>
2015-12-01 21:50:43 +0400

    show confirm message before removing doc and fix bug
    show confirm message before removing doc and fix bug

--- public/modules/uploads/controller/index.upload.controller.js
+++ public/modules/uploads/controller/index.upload.controller.js
@@ -7,13 +7,18 @@ angular.module('uploadModule').controller('indexUploadController', ['$scope', '$
 		$modalInstance.dismiss('cancel');
 	}
 
-	$http.get('/api/v1/case/' + $scope.selectedCase._id + '/docs')
-	.success(function(data, success){
-		$scope.docs = data;
-	})
-	.error(function(data, error){
-		$scope.error = error.data;
-	});
+	$scope.allDocs = function(){
+		$http.get('/api/v1/case/' + $scope.selectedCase._id + '/docs')
+		.success(function(data, success){
+			$scope.docs = data;
+		})
+		.error(function(data, error){
+			$scope.error = error.data;
+		});	
+	}
+
+	//init get all docs
+	$scope.allDocs();
 
 	$scope.addDoc = function(){
 		$modal.open({
@@ -36,9 +41,51 @@ angular.module('uploadModule').controller('indexUploadController', ['$scope', '$
 	}
 
 	$scope.removeDoc = function (index, id) {
-		connectCaseFactory.delete({'caseId': selectedCase._id, 'action': 'upload', 'actionId': id}, function(response){
-			$scope.docs = response.docs;
+		var modalInstance = $modal.open({
+			templateUrl: 'public/modules/config/view/message/confirm.message.config.view.html',
+			backdrop: 'static',
+			controller: ['$scope', '$modalInstance', 'doc', 'selectedCase', 'allDocs', function($scope, $modalInstance, doc, selectedCase, allDocs){
+				$scope.message = {};
+				$scope.message.title = 'حذف المستند';
+				$scope.message.text = ' هل ترغب بحذف المستند ' + doc.name  + ' ?';
+				$scope.message.confirm = 'نعم';
+				$scope.message.cancel = 'لا';
+
+				$scope.confirm = function(){
+					connectCaseFactory.delete({'caseId': selectedCase._id, 'action': 'upload', 'actionId': id}, function(response){
+						allDocs();
+						$modalInstance.dismiss('cancel');
+					}, function (error) {
+						$scope.error = error
+					});
+				}
+
+				$scope.cancel = function(){
+					$modalInstance.dismiss('cancel');
+				}
+
+				$scope.closeModal = function(){
+					$modalInstance.dismiss('cancel');
+				}
+			}],
+			resolve: {
+				doc: function(){
+					return $scope.docs[index];
+				},
+				allDocs: function () {
+					return $scope.allDocs;
+				},
+				selectedCase: function(){
+					return $scope.selectedCase;
+				}
+			}
 		});
+
+
+
+
+
+
 	}
 
 }]);
